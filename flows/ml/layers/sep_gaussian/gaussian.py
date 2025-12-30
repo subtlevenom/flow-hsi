0f5dc7a60539eed64d7964b05ec6e62bcc67abea
subtlevenom <andrew.korepanov@gmail.com>
2025-12-31 02:14:21 +0400

    fixed gaussian

--- flows/ml/layers/sep_gaussian/gaussian.py
+++ flows/ml/layers/sep_gaussian/gaussian.py
@@ -8,6 +8,6 @@ class Gaussian(torch.nn.Module):
 
     def forward(self, x:torch.Tensor, a:torch.Tensor, m:torch.Tensor, s:torch.Tensor):
         x = (x - m) * s # BCHW
-        x = torch.sum(x, dim=1, keepdim=True)
+        x = torch.sum(x*x, dim=1, keepdim=True)
         x = a * torch.exp(-0.5 * x)
         return x
