d422f0a1e1aed98fc66a2b1d03fcbf6ab2104913
subtlevenom <andrew.korepanov@gmail.com>
2026-03-15 12:54:11 +0400

    fixes

---
+++ flows/ml/models/ggpd/gp_replicate.py
@@ -0,0 +1,33 @@
+from typing import List, Union
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange, einsum
+from flows.ml.layers.encoders import CMEncoder, LightCMEncoder
+from flows.ml.layers.encoders.sg_encoder import FFN, LayerNorm
+from flows.ml.layers.sep_gpd import MultivariateNormal
+from flows.ml.layers.sep_gpd import SepGPD, SepGPDLayer
+
+
+class GPReplicate(nn.Module):
+
+    def __init__(
+        self,
+        n: int = 0,
+        dim: int=0,
+        **kwargs,
+    ):
+        super(GPSplit, self).__init__()
+        self.n = n
+        self.dim=dim
+
+    def forward(
+        self,
+        x: torch.Tensor,
+    ):
+        """ 
+        x: BCHW -> NBCHW
+        """
+
+        x = torch.stack([x]*self.n, dim=self.dim)
+        return x
