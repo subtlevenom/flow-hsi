8b2899c916badfeaf809f8bfd30187343dcdde46
subtlevenom <andrew.korepanov@gmail.com>
2026-03-31 02:40:03 +0400

    scripts

---
+++ scripts/cli-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+CONTAINER_NAME=flow-hsi
+
+docker build --tag=$(id -u -n)/$CONTAINER_NAME:latest --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) ./
---
+++ scripts/cli-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CONTAINER_NAME=flow-hsi
+IMAGE_NAME=$(id -u -n)/flow-hsi:latest
+
+WORK_PATH=~/work/flow-hsi
+DATA_PATH=/data/korepanov/datasets
+
+#-u $(id -u):$(id -g) \
+docker run --rm -it \
+    --runtime=nvidia --gpus all \
+    -p 8888:8888 -p 6006:6006 \
+    -v $WORK_PATH:/flow-hsi \
+    -v $DATA_PATH:/data \
+    --name $CONTAINER_NAME \
+    $IMAGE_NAME
