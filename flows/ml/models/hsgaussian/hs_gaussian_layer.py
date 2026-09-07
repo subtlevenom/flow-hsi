29e799b2adc472a39a87724499ced2f4dd67101d
subtlevenom <andrew.korepanov@gmail.com>
2026-09-08 03:08:48 +0400

    fixes

--- flows/ml/models/hsgaussian/hs_gaussian_layer.py
+++ flows/ml/models/hsgaussian/hs_gaussian_layer.py
@@ -3,6 +3,7 @@ from typing import List
 import numpy as np
 import torch
 from torch import nn
+from torch.utils.checkpoint import checkpoint
 from .hs_gaussian import HSGaussian
 
 
@@ -12,15 +13,22 @@ class HSGaussianLayer(nn.Module):
         self,
         in_channels: int,
         out_channels: int,
+        use_checkpoint: bool = True,
     ):
         super(HSGaussianLayer, self).__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
+        self.use_checkpoint = use_checkpoint
 
         self.layers = nn.ModuleList(
             [HSGaussian(in_channels) for _ in range(out_channels)])
 
     def forward(self, x:torch.Tensor):
-        y = torch.concat([g(x) for g in self.layers], dim=1)
+        if self.use_checkpoint and self.training and torch.is_grad_enabled():
+            y = torch.concat(
+                [checkpoint(g, x, use_reentrant=False) for g in self.layers],
+                dim=1)
+        else:
+            y = torch.concat([g(x) for g in self.layers], dim=1)
         return y
--- flows/ml/models/hskan/hs_layer.py
+++ flows/ml/models/hskan/hs_layer.py
@@ -2,6 +2,7 @@ from typing import List
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.checkpoint import checkpoint
 from einops import rearrange
 from flows.ml.layers.encoders.sg_encoder import FFN, LayerNorm
 from ..hsgaussian import HSGaussianLayer
@@ -14,20 +15,27 @@ class HSLayer(nn.Module):
         in_channels: int = 3,
         out_channels: int = 3,
         n_layers: int = 7,
+        use_checkpoint: bool = True,
     ):
         super(HSLayer, self).__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
+        self.use_checkpoint = use_checkpoint
 
         self.layer = HSGaussianLayer(in_channels, n_layers)
         self.norm = LayerNorm(in_channels)
         self.ffn = FFN(in_channels=n_layers + 2 * in_channels,
                        out_channels=in_channels)
 
-    def forward(self, x: torch.Tensor):
+    def _forward(self, x: torch.Tensor):
         z = self.norm(x)
         y = self.layer(x)
         y = torch.concat([x, z, y], dim=1)
         y = self.ffn(y)
         return y
+
+    def forward(self, x: torch.Tensor):
+        if self.use_checkpoint and self.training and torch.is_grad_enabled():
+            return checkpoint(self._forward, x, use_reentrant=False)
+        return self._forward(x)
