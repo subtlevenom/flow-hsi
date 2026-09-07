e813ccfc5cad4dfff5582c0babd5b2057fcfcd30
subtlevenom <andrew.korepanov@gmail.com>
2026-09-08 02:51:30 +0400

    fixes

--- flows/ml/models/hsgaussian/hs_gaussian.py
+++ flows/ml/models/hsgaussian/hs_gaussian.py
@@ -5,11 +5,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 from flows.ml.layers.sep_gaussian import SepGaussian
 from flows.ml.layers.encoders import SGEncoder, CMEncoder, LightCMEncoder
+from flows.ml.layers.mst import MST_Plus_Plus
 
 
 class HSGaussian(SepGaussian):
 
     def create_encoder(self, in_channels, out_channels, **kwargs):
+        return MST_Plus_Plus(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            n_feat=20,
+            stage=2
+        )
         return LightCMEncoder(in_channels, out_channels)
 
     def forward(self, x):
